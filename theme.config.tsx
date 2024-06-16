297972e722f783d7400ba4814f2683600bea2994
GeorgII <george.webfullstack@gmail.com>
2024-06-16 22:42:45 +0200

    Update Google site verification method
    Removed the separate HTML Google verification file and instead added the verification meta tag directly into the theme configuration. This allows for smoother verification process with less clutter in the project directory.

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,6 +9,7 @@ const config: DocsThemeConfig = {
     docsRepositoryBase: 'https://github.com/GeorgII-web/notes/blob/master/',
     head: <>
         <meta name="description" content="IT notes and articles on PHP, Symfony, Pimcore and related topics."/>
+        <meta name="google-site-verification" content="JKtg18pK8fbTGPfTZKJ5B3N-Mn6rMX1636TpJAWFbs8"/>
     </>,
     footer: {
         text: <p>github.com/GeorgII-web | t.me/GeorgiiW</p>,
